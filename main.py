3da2e66bd140e95418708e1c3ca140cf0251b22c
MeikeB <meike.buysse@gmail.com>
2025-07-16 14:24:26 +0200

    Added FPS config and basic endscreen

--- main.py
+++ main.py
@@ -4,12 +4,15 @@ from entities.player.player import Player
 from entities.asteroid.asteroid import Asteroid
 from entities.asteroid.asteroidfield import AsteroidField
 from entities.projectile.projectile import Projectile
+from core.config import FPS
 
 def main():
 
     pygame.init()
 
     screen = pygame.display.set_mode((controller.CURRENT_SCREEN_WIDTH, controller.CURRENT_SCREEN_HEIGHT))
+    pygame.display.set_caption("Astroids")
+
     # Making sure the initial values match
     controller.CURRENT_SCREEN_WIDTH = screen.get_width()
     controller.CURRENT_SCREEN_HEIGHT = screen.get_height()
@@ -32,26 +35,30 @@ def main():
     player = Player(controller.CURRENT_SCREEN_WIDTH / 2, controller.CURRENT_SCREEN_HEIGHT / 2)
     asteroidfield = AsteroidField()
 
-    while True:
+    game_running = True
+    while game_running:
         # Code to check for events
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                return
+                game_running = False
+                break
             elif event.type == pygame.VIDEORESIZE:
                 screen = pygame.display.set_mode(event.size, pygame.RESIZABLE)
                 controller.CURRENT_SCREEN_WIDTH = event.size[0]
                 controller.CURRENT_SCREEN_HEIGHT = event.size[1]
-            
+        
+        # Update the entities
         updatable.update(dt)
 
+        # Check for collision
         for a in asteroids:
             for shot in projectiles:
                 if a.hasCollided(shot):
                     shot.kill()
                     a.split()
             if a.hasCollided(player):
-                print("Game over!")
-                exit()
+                game_running = False
+                break
         
         # Rendering the screen
         screen.fill("black")
@@ -60,7 +67,32 @@ def main():
         pygame.display.flip()
 
         # Measuring Ticks
-        dt = clock.tick(60)/1000
+        dt = clock.tick(FPS)/1000
+    
+    # Exit screen
+    my_font = pygame.font.SysFont('Comic Sans MS', 30)
+    exit_text = my_font.render('Game Over!', False, (255, 255, 255))
+    text_position = exit_text.get_rect()
+    text_position.center = (controller.CURRENT_SCREEN_WIDTH // 2, controller.CURRENT_SCREEN_HEIGHT // 2)
+
+    show_endscreen = True
+    while show_endscreen:
+        # Code to check for events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                show_endscreen = False
+                break
+
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_SPACE] or keys[pygame.K_KP_ENTER] or keys[pygame.K_ESCAPE]:
+            show_endscreen = False
+            break
+
+        # Rendering the screen
+        screen.fill("black")
+        screen.blit(exit_text, text_position)
+        pygame.display.flip()
+    
 
 if __name__ == "__main__":
     main()
